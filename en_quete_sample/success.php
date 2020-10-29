8536ec5c3416005ae337fb3033fb22c640fe5c74
Ponpoyo <rodeoxy23he1@gmail.com>
2020-10-29 21:50:48 +0900

    1st commit

---
+++ en_quete_sample/success.php
@@ -0,0 +1,17 @@
+<html>
+<head><title>送信完了</title></head>
+<body>
+<?php 
+
+    // 標準時設定
+    date_default_timezone_set('Asia/Tokyo');
+    
+    // 時間表記
+    $time = date("Y/m/d H:i:s");
+    print($time."<br/>");
+
+    print("正常に送信されました。"."<br/>");
+
+?>
+</body>
+</html>
